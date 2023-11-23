1184c2b106dc075e8b2e7aaeb5dc5a4e7522f868
VladyslavKosarenko <131978971+VladyslavKosarenko@users.noreply.github.com>
2023-11-23 02:39:19 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Button } from './Button'; 
 import { Modal } from './Modal'; 
@@ -15,44 +15,45 @@ const StyledApp = styled.div`
   padding-bottom: 24px;
 `;
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    currentPage: 1,
-    isLoading: false,
-    selectedImage: null,
-    totalHits: null,
-  };
+export const App = () => {
 
-  async componentDidMount() {
-    this.setState({ isLoading: true });
-    try {
-      const initialImages = await this.fetchImages();
-      this.setState({
-        images: initialImages,
-      });
-    } catch (error) {
-      console.error('Error fetching initial images:', error);
-    } finally {
-      this.setState({ isLoading: false });
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [totalHits, setTotalHits] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  useEffect(() => {
+    async function getImages() {
+      try {
+        setIsLoading(true);
+        const initialImages = await fetchImages();
+        setImages(initialImages);
+      } catch (error) {
+        console.error('Error fetching initial images:', error);
+      } finally {
+        setIsLoading(false);
+      }
     }
+    getImages()
+  }, []);
+  
+useEffect(() => {
+  if (query === '') {
+    return;
   }
+    fetchImages(); 
+}, [query]);
+  
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, currentPage } = this.state;
-
-    if (prevState.query !== query || prevState.currentPage !== currentPage) {
-      this.fetchImages();
-    }
-  }
-
-  fetchImages = async () => {
-    const { query, currentPage } = this.state;
+  const fetchImages = async () => {
+    
     const apiKey = '40677979-cd292d53039c37b7a3f8f64ae';
 
     try {
-      this.setState({ isLoading: true });
+      setIsLoading(true);
 
       const response = await axios.get(`https://pixabay.com/api/`, {
         params: { key: apiKey, q: query, page: currentPage, per_page: 12 },
@@ -60,66 +61,68 @@ export class App extends Component {
 
       const newImages = response.data.hits;
       const totalMaxHits = response.data.totalHits
-      this.setState((prevState) => ({
-        images: [...prevState.images, ...newImages],
-        totalHits: totalMaxHits,
-      }));
-      return newImages; 
+      setImages(prevState => [...prevState, ...newImages]);
+      setTotalHits(totalMaxHits);
+      
+      return newImages;
     } catch (error) {
       console.error('Error fetching images:', error);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  handleSearch = (newQuery) => {
-    this.setState({ query: newQuery, images: [], currentPage: 1 });
+  const handleSearch = (newQuery) => {
+    setQuery(newQuery);
+    setImages([]);
+    setCurrentPage(1);
   };
 
-  loadMore = () => {
-    this.setState((prevState) => ({ currentPage: prevState.currentPage + 1 }));
+  const loadMore = () => {
+    setCurrentPage(prevState => prevState + 1);
   };
 
-openModal = (image) => {
-  if (!this.state.isModalOpen) {
-    this.setState({ selectedImage: image, isModalOpen: true });
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
-};
+  const openModal = (image) => {
+    if (!isModalOpen) {
+      setSelectedImage(image);
+      setIsModalOpen(true);
+      document.addEventListener('keydown', handleKeyDown);
+    }
+  };
 
-closeModal = () => {
-  if (this.state.isModalOpen) {
-    this.setState({ selectedImage: null, isModalOpen: false });
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
-};
+  const closeModal = () => {
+    if (isModalOpen) {
+      setSelectedImage(null);
+      setIsModalOpen(false);
+      document.removeEventListener('keydown', handleKeyDown);
+    }
+  };
 
-  handleKeyDown = (e) => {
+  const handleKeyDown = (e) => {
     if (e.key === 'Escape') {
-      this.closeModal();
+      closeModal();
     }
   };
 
-  render() {
-    const { images, isLoading, selectedImage } = this.state;
+  
+    
 
-    return (
-      <StyledApp className="App">
-        <Searchbar onSubmit={this.handleSearch} />
-        <ImageGallery images={images} onImageClick={this.openModal}>
-          {images.map((image) => (
-            <ImageGalleryItem key={image.id} id={image.id} src={image.webformatURL} alt={image.tags} onClick={() => this.openModal(image)} />
-          ))}
-        </ImageGallery>
-        {isLoading && <Bars type="Oval" color="#00BFFF" height={100} width={100} timeout={3000} />}
-        {images.length > 0 && !isLoading && this.state.totalHits > images.length && <Button onLoadMore={this.loadMore} show={true} />}
-        {selectedImage && (
-          <Modal
-            image={selectedImage.largeImageURL}
-            onClose={this.closeModal}
-          />
-        )}
-      </StyledApp>
-    );
-  }
+  return (
+    <StyledApp className="App">
+      <Searchbar onSubmit={handleSearch} />
+      <ImageGallery images={images} onImageClick={openModal}>
+        {images.map((image) => (
+          <ImageGalleryItem key={image.id} id={image.id} src={image.webformatURL} alt={image.tags} onClick={() => openModal(image)} />
+        ))}
+      </ImageGallery>
+      {isLoading && <Bars type="Oval" color="#00BFFF" height={100} width={100} timeout={3000} />}
+      {images.length > 0 && !isLoading && totalHits > images.length && <Button onLoadMore={loadMore} show={true} />}
+      {selectedImage && (
+        <Modal
+          image={selectedImage.largeImageURL}
+          onClose={closeModal}
+        />
+      )}
+    </StyledApp>
+  );
 }
